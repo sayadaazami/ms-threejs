18241b371ea48c90d72a485fdced0674eda684f8
sayadaazami <sayadaazami@gmail.com>
2024-06-02 17:02:43 +0000

    003-request-animation-frame3

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const rootElement = document.querySelector("#root");
 let elapsedTime = 0;
 let startTime = Date.now();
+const width = window.innerWidth - rootElement.offsetWidth;
 
 const looper = () => {
-  rootElement.style.transform = `rotate(${elapsedTime / 1000 * 6}deg)`;
-
+  const x = elapsedTime / 1000;
+  const y = (1 + Math.sin(x)) * width /2;
+  rootElement.style.transform = `translateX(${y}px)`;
   elapsedTime = Date.now() - startTime;
   requestAnimationFrame(looper);
 };
